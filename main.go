1d85d80ba581f931dca92c620ad57761225e463c
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-04-09 17:05:01 -0600

    set up server connection to database
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-pg/pg"
 )
+
+type Course struct {
+	Id          int
+	Name        string
+	Institution string
+	CreditHours int
+	Length      int
+	CreatedAt   string
+	UpdatedAt   string
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/courses/1", func(c *gin.Context) {
-		c.String(200, "Nursing 101")
+		db := pg.Connect(&pg.Options{
+			User:     "postgres",
+			Database: "course_chart",
+		})
+		defer db.Close()
+
+		course := &Course{Id: 1}
+		err := db.Model(course).WherePK().Select()
+		if err != nil {
+			panic(err)
+		}
+
+		c.String(200, course.CreatedAt)
 	})
 	return r
 }
