d169cb113efeb8fb8827b17325a265dce854badb
Ely Hess <ely.hess@me.com>
2021-04-12 17:20:09 -0600

    Add modules to course model
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- models/course.go
+++ models/course.go
@@ -1,11 +1,12 @@
 package models
 
 type Course struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Institution string `json:"institution"`
-	CreditHours int    `json:"creditHours"`
-	Length      int    `json:"length"`
-	CreatedAt   string `json:"createdAt"`
-	UpdatedAt   string `json:"updatedAt"`
+	Id          int      `json:"id"`
+	Name        string   `json:"name"`
+	Institution string   `json:"institution"`
+	CreditHours int      `json:"creditHours"`
+	Length      int      `json:"length"`
+	CreatedAt   string   `json:"-"`
+	UpdatedAt   string   `json:"-"`
+	Modules     []Module `json:"modules"`
 }
