d5cf1bf23a324d3964d90ba657514e67e33819eb
Ely Hess <ely.hess@me.com>
2021-04-12 15:24:31 -0600

    Refactor handlers
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- routes/routes.go
+++ routes/routes.go
@@ -1,30 +1,13 @@
 package routes
 
 import (
-	db "course-chart/config"
+	"course-chart/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Course struct {
-	Id          int
-	Name        string
-	Institution string
-	CreditHours int
-	Length      int
-	CreatedAt   string
-	UpdatedAt   string
-}
-
 func GetRoutes() *gin.Engine {
-	r := gin.Default()
-	r.GET("/courses/1", GetCourse)
-	return r
-}
-
-func GetCourse(c *gin.Context) {
-	var course Course
-	db.Conn.First(&course, 1)
-
-	c.String(200, course.Name)
+	router := gin.Default()
+	router.GET("/courses/1", handlers.GetCourse)
+	return router
 }
