898c61ff3a994051e6ccd0d9debf8071874c66b9
Ely Hess <ely.hess@me.com>
2021-04-12 12:31:21 -0600

    Refactor test to use TestMode DB connection
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"database/sql"
+	"course-chart/routes"
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"course-chart/config"
+
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func TestGETCourses(t *testing.T) {
-	db := dbConnect()
+	gin.SetMode(gin.TestMode)
+	config.Connect()
 
 	t.Run("returns the name of a course", func(t *testing.T) {
-		course := &Course{
+		course := &routes.Course{
 			Id:   1,
 			Name: "Foundations of Nursing",
 		}
 
-		router := setupRouter(db)
+		router := routes.GetRoutes()
 		request, _ := http.NewRequest("GET", fmt.Sprintf("/courses/%d", course.Id), nil)
 		response := httptest.NewRecorder()
 
@@ -31,28 +32,4 @@ func TestGETCourses(t *testing.T) {
 		assert.Equal(t, 200, response.Code)
 		assert.Equal(t, course.Name, response.Body.String())
 	})
-}
-
-func dbConnect() *gorm.DB {
-	const (
-		host   = "localhost"
-		port   = 5432
-		user   = "postgres"
-		dbname = "course_chart_test"
-	)
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"dbname=%s sslmode=disable",
-		host, port, user, dbname)
-
-	sqlDB, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		log.Fatalf("Error opening database: %q", err)
-	}
-
-	gormDB, err := gorm.Open(postgres.New(postgres.Config{
-		Conn: sqlDB,
-	}), &gorm.Config{})
-
-	return gormDB
 }
