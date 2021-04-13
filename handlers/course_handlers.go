bdfe667ec993ff3f389a57148f83ed3add5fe841
Ely Hess <ely.hess@me.com>
2021-04-12 20:31:15 -0600

    Add RenderError function
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- handlers/course_handlers.go
+++ handlers/course_handlers.go
@@ -3,14 +3,28 @@ package handlers
 import (
 	db "course-chart/config"
 	"course-chart/models"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func RenderError(c *gin.Context, err error) {
+	log.Printf("Error retrieving course from database.\nReason: %v", err)
+	c.JSON(http.StatusNotFound, gin.H{
+		"status": http.StatusNotFound,
+		"errors": "Course not found",
+	})
+}
+
 func GetCourse(c *gin.Context) {
 	var course models.Course
-	db.Conn.Preload("Modules.ModuleActivities.Activity").First(&course, c.Param("id"))
+	err := db.Conn.Preload("Modules.ModuleActivities.Activity").First(&course, c.Param("id")).Error
+
+	if err != nil {
+		RenderError(c, err)
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"status":  http.StatusOK,
