24d5c25e4fc5a6af23a79f31cbe7b8e8811e585b
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-04-13 21:35:18 -0600

    add activityTotals to response for courses/:id endpoint

--- handlers/course_handlers.go
+++ handlers/course_handlers.go
@@ -19,17 +19,34 @@ func RenderError(c *gin.Context, err error) {
 
 func GetCourse(c *gin.Context) {
 	var course models.Course
-	err := db.Conn.Preload("Modules.ModuleActivities.Activity").First(&course, c.Param("id")).Error
+	err := db.Conn.First(&course, c.Param("id")).Error
 
 	if err != nil {
 		RenderError(c, err)
 		return
 	}
 
+	var activityTotals []models.ActivityTotals
+	db.Conn.Model(&models.Activity{}).
+		Joins("JOIN module_activities ON module_activities.activity_id=activities.id").
+		Joins("JOIN modules ON modules.id=module_activities.module_id").
+		Select("activities.name, activities.id, modules.id AS module_id, modules.name AS module_name, sum(multiplier * module_activities.input) AS minutes").
+		Group("activities.id, modules.id, modules.name").
+		Where("modules.course_id = ?", c.Param("id")).
+		Scan(&activityTotals)
+
+	completeResponse := struct {
+		Course         models.Course           `json:"course"`
+		ActivityTotals []models.ActivityTotals `json:"activityTotals"`
+	}{
+		Course:         course,
+		ActivityTotals: activityTotals,
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"status":  http.StatusOK,
 		"message": "Course found",
-		"data":    course,
+		"data":    completeResponse,
 	})
 }
 
--- models/course.go
+++ models/course.go
@@ -1,14 +1,13 @@
 package models
 
 type Course struct {
-	Id          int      `json:"id"`
-	Name        string   `json:"name"`
-	Institution string   `json:"institution"`
-	CreditHours int      `json:"creditHours"`
-	Length      int      `json:"length"`
-	CreatedAt   string   `json:"-"`
-	UpdatedAt   string   `json:"-"`
-	Modules     []Module `json:"modules"`
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Institution string `json:"institution"`
+	CreditHours int    `json:"creditHours"`
+	Length      int    `json:"length"`
+	CreatedAt   string `json:"-"`
+	UpdatedAt   string `json:"-"`
 }
 
 type CourseIdentifier struct {
