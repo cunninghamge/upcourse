82f3915eb73684515b43841ecaf1bacdfe073ee1
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-04-14 14:30:33 -0600

    change data type of ActivityTotals minutes from int to float
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- models/activity.go
+++ models/activity.go
@@ -14,9 +14,9 @@ type Activity struct {
 }
 
 type ActivityTotals struct {
-	Id         int    `json:"activityId"`
-	Name       string `json:"activityName"`
-	ModuleId   int    `json:"moduleId"`
-	ModuleName string `json:"moduleName"`
-	Minutes    int    `json:"minutes"`
+	Id         int     `json:"activityId"`
+	Name       string  `json:"activityName"`
+	ModuleId   int     `json:"moduleId"`
+	ModuleName string  `json:"moduleName"`
+	Minutes    float32 `json:"minutes"`
 }
