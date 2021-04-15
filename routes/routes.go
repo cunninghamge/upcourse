6de14be6d5248e9d2ee7625abc5ea7d643bb67fd
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-04-14 21:26:11 -0600

    add route for POST /courses
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- routes/routes.go
+++ routes/routes.go
@@ -13,8 +13,10 @@ func GetRoutes() *gin.Engine {
 	router.Use(cors.Default())
 
 	router.GET("/courses", handlers.GetCourses)
+	router.POST("/courses", handlers.CreateCourse)
 	router.GET("/courses/:id", handlers.GetCourse)
 	router.GET("/modules/:id", handlers.GetModule)
 	router.GET("/activities", handlers.GetActivities)
+
 	return router
 }
