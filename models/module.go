6db1c2a2b791b3bd34612ee3c18d1d6b9f96c502
Ely Hess <ely.hess@me.com>
2021-04-15 16:02:47 -0600

    Add binding:required to module struct
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- models/module.go
+++ models/module.go
@@ -4,8 +4,8 @@ import "time"
 
 type Module struct {
 	ID               int              `json:"id"`
-	Name             string           `json:"name"`
-	Number           int              `json:"number"`
+	Name             string           `json:"name" binding:"required"`
+	Number           int              `json:"number" binding:"required"`
 	CourseId         int              `json:"courseId"`
 	CreatedAt        time.Time        `json:"-" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	UpdatedAt        time.Time        `json:"-" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
