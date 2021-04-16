44b2c2e79d9f6fd02d742e2379b3e00101ec235a
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-04-15 21:28:16 -0600

    move get_module_test to testing package

---
+++ testing/get_module_test.go
@@ -0,0 +1,88 @@
+package testing
+
+import (
+	"course-chart/models"
+	"course-chart/routes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGETModule(t *testing.T) {
+	mockModule := newMockModule()
+	defer teardown()
+
+	t.Run("returns a module", func(t *testing.T) {
+		response := newGetModuleRequest(mockModule.ID)
+
+		assert.Equal(t, 200, response.Code)
+
+		parsedResponse := unmarshalGETModule(t, response.Body)
+
+		assertResponseValue(t, parsedResponse.Message, "Module found", "Response message")
+		assertResponseValue(t, parsedResponse.Data.ID, mockModule.ID, "Id")
+		assertResponseValue(t, parsedResponse.Data.Name, mockModule.Name, "Name")
+		assertResponseValue(t, parsedResponse.Data.Number, mockModule.Number, "Number")
+		assertResponseValue(t, parsedResponse.Data.CourseId, mockModule.CourseId, "CourseId")
+		firstResponseModActivity := parsedResponse.Data.ModuleActivities[0]
+		firstMockModActivity := mockModule.ModuleActivities[0]
+		assertResponseValue(t, firstResponseModActivity.Input, firstMockModActivity.Input, "Module Activity Input")
+		assertResponseValue(t, firstResponseModActivity.Notes, firstMockModActivity.Notes, "Module Activity Notes")
+		firstResponseActivity := firstResponseModActivity.Activity
+		firstMockActivity := firstMockModActivity.Activity
+		assertResponseValue(t, firstResponseActivity.ID, firstMockActivity.ID, "Activity Id")
+		assertResponseValue(t, firstResponseActivity.Description, firstMockActivity.Description, "Activity Description")
+		assertResponseValue(t, firstResponseActivity.Metric, firstMockActivity.Metric, "Activity Metric")
+		assertResponseValue(t, firstResponseActivity.Multiplier, firstMockActivity.Multiplier, "Activity Multiplier")
+	})
+
+	t.Run("returns a message if the module is not found", func(t *testing.T) {
+		response := newGetModuleRequest(mockModule.ID + 1)
+
+		assert.Equal(t, 404, response.Code)
+
+		parsedResponse := UnmarshalError(t, response.Body)
+
+		assertResponseValue(t, parsedResponse.Errors, "Record not found", "Response message")
+	})
+}
+
+func newGetModuleRequest(moduleId int) *httptest.ResponseRecorder {
+	router := routes.GetRoutes()
+	request, _ := http.NewRequest("GET", fmt.Sprintf("/modules/%d", moduleId), nil)
+	response := httptest.NewRecorder()
+
+	router.ServeHTTP(response, request)
+
+	return response
+}
+
+func unmarshalGETModule(t *testing.T, response io.Reader) getModuleResponse {
+	t.Helper()
+	body, _ := ioutil.ReadAll(response)
+
+	responseModule := getModuleResponse{}
+	err := json.Unmarshal([]byte(body), &responseModule)
+	if err != nil {
+		t.Errorf("Error marshaling JSON response\nError: %v", err)
+	}
+
+	if reflect.DeepEqual(responseModule.Data, models.Module{}) {
+		t.Errorf("response does not contain an id property")
+	}
+
+	return responseModule
+}
+
+type getModuleResponse struct {
+	Data    models.Module
+	Message string
+	Status  int
+}
