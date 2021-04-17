63e42aa23f328e8d85b77267e7d00258ae32ad29
Ely Hess <ely.hess@me.com>
2021-04-17 12:02:40 -0600

    Add delete module test
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

---
+++ testing/delete_module_test.go
@@ -0,0 +1,62 @@
+package testing
+
+import (
+	"course-chart/config"
+	"course-chart/models"
+	"course-chart/routes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDELETEmodule(t *testing.T) {
+	mockModule := newModule()
+	defer teardown()
+
+	t.Run("Deletes a module and its children", func(t *testing.T) {
+		var moduleCount int64
+		config.Conn.Model(models.Module{}).Count(&moduleCount)
+
+		response := newDeleteModuleRequest(mockModule.ID)
+
+		assert.Equal(t, 200, response.Code)
+
+		parsedResponse := UnmarshalSuccess(t, response.Body)
+
+		assertResponseValue(t, parsedResponse.Message, "Module deleted successfully", "Message")
+
+		var newModuleCount int64
+		config.Conn.Model(models.Module{}).Count(&newModuleCount)
+
+		if moduleCount == newModuleCount {
+			t.Errorf("Did not delete module")
+		}
+
+		err := config.Conn.First(&models.Module{}, mockModule.ID).Error
+		if err == nil {
+			t.Errorf("Also did not delete module")
+		}
+
+		err = config.Conn.First(&models.ModuleActivity{}, mockModule.ModuleActivities[0].ID).Error
+		if err == nil {
+			t.Errorf("Did not delete associated module activities")
+		}
+
+	})
+
+}
+
+func newDeleteModuleRequest(moduleId int) *httptest.ResponseRecorder {
+	router := routes.GetRoutes()
+	request, _ := http.NewRequest("DELETE", fmt.Sprintf("/modules/%d", moduleId), nil)
+	response := httptest.NewRecorder()
+
+	request.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(response, request)
+
+	return response
+}
