ea856c837fec0c58a830cb7587941a47e20e78e4
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-04-17 18:57:04 -0600

    add new validation tags to course and module and remove unneeded structs

--- models/course.go
+++ models/course.go
@@ -4,20 +4,11 @@ import "time"
 
 type Course struct {
 	ID                   int       `json:"id"`
-	Name                 string    `json:"name" binding:"required"`
-	Institution          string    `json:"institution,omitempty" binding:"required"`
-	CreditHours          int       `json:"creditHours,omitempty" binding:"required"`
-	Length               int       `json:"length,omitempty" binding:"required"`
+	Name                 string    `json:"name" validate:"onCreate"`
+	Institution          string    `json:"institution,omitempty" validate:"onCreate"`
+	CreditHours          int       `json:"creditHours,omitempty" validate:"onCreate"`
+	Length               int       `json:"length,omitempty" validate:"onCreate"`
 	Goal                 string    `json:"goal,omitempty"`
 	CreatedAt, UpdatedAt time.Time `json:"-" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	Modules              []Module  `json:"modules"`
-}
-
-type UpdatableCourse struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Institution string `json:"institution"`
-	CreditHours int    `json:"creditHours"`
-	Length      int    `json:"length"`
-	Goal        string `json:"goal"`
 }
--- models/module.go
+++ models/module.go
@@ -4,16 +4,9 @@ import "time"
 
 type Module struct {
 	ID                   int              `json:"id"`
-	Name                 string           `json:"name" binding:"required"`
-	Number               int              `json:"number,omitempty" binding:"required"`
+	Name                 string           `json:"name" validate:"onCreate"`
+	Number               int              `json:"number,omitempty" validate:"onCreate"`
 	CourseId             int              `json:"courseId"`
 	CreatedAt, UpdatedAt time.Time        `json:"-" gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	ModuleActivities     []ModuleActivity `json:"moduleActivities,omitempty"`
-}
-
-type UpdatableModule struct {
-	ID               int              `json:"id"`
-	Name             string           `json:"name"`
-	Number           int              `json:"number"`
-	ModuleActivities []ModuleActivity `json:"moduleActivities"`
 }
