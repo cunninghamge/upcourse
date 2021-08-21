e3adecdcbf34fd61ad66715c954be2a40f189d89
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-08-20 22:30:05 -0600

    move modules db logic to models

--- handlers/module_handlers.go
+++ handlers/module_handlers.go
@@ -8,46 +8,42 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	db "upcourse/database"
 	"upcourse/models"
 )
 
 const moduleSchema = "./schemas/module_schema.json"
 
 func GetModule(c *gin.Context) {
-	var module models.Module
-	tx := db.Conn.Preload("ModuleActivities.Activity").First(&module, c.Param("id"))
-	if tx.Error != nil {
-		renderError(c, tx.Error)
+	module, err := models.GetModule(c.Param("id"))
+	if err != nil {
+		renderError(c, err)
 		return
 	}
 
-	renderFoundRecords(c, &module)
+	renderFoundRecords(c, module)
 }
 
 func CreateModule(c *gin.Context) {
+	courseId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		renderError(c, errors.New(ErrBadRequest))
+		return
+	}
+
 	jsonData, errs := models.Validate(c, moduleSchema)
 	if errs != nil {
 		renderErrors(c, errs)
 		return
 	}
 
-	var module models.Module
+	module := models.Module{CourseId: courseId}
 	if err := json.Unmarshal(jsonData, &module); err != nil {
 		renderError(c, err)
 		return
 	}
 
-	courseId, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		renderError(c, errors.New(ErrBadRequest))
-		return
-	}
-	module.CourseId = courseId
-
-	tx := db.Conn.Create(&module)
-	if tx.Error != nil {
-		renderError(c, tx.Error)
+	if err := models.CreateModule(&module); err != nil {
+		renderError(c, err)
 		return
 	}
 
@@ -61,31 +57,17 @@ func UpdateModule(c *gin.Context) {
 		return
 	}
 
-	tx := db.Conn.Model(&models.Module{}).First(&models.Module{}, c.Param("id")).Updates(&module)
-	if tx.Error != nil {
-		renderError(c, tx.Error)
+	if err := models.UpdateModule(&module, c.Param("id")); err != nil {
+		renderError(c, err)
 		return
-	}
-
-	moduleId, _ := strconv.Atoi(c.Param("id"))
-	for _, ma := range module.ModuleActivities {
-		ma.ModuleId = moduleId
-		tx = db.Conn.Where(models.ModuleActivity{ModuleId: moduleId, ActivityId: ma.ActivityId}).
-			FirstOrCreate(&models.ModuleActivity{}).
-			Updates(&ma)
-		if tx.Error != nil {
-			renderError(c, tx.Error)
-			return
-		}
 	}
 
 	c.JSON(http.StatusOK, nil)
 }
 
 func DeleteModule(c *gin.Context) {
-	tx := db.Conn.Delete(&models.Module{}, c.Param("id"))
-	if tx.Error != nil {
-		renderError(c, tx.Error)
+	if err := models.DeleteModule(c.Param("id")); err != nil {
+		renderError(c, err)
 		return
 	}
 
